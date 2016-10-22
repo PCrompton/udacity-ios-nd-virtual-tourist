3d970ac4c844f3cf55f12f3cd064d65d22e1f8f5
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-10-21 21:26:42 -0400

    Added my Flickr API key to Constants.swift

--- Virtual Tourist/Constants.swift
+++ Virtual Tourist/Constants.swift
@@ -41,7 +41,7 @@ struct Constants {
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "YOUR_API_KEY_HERE"
+        static let APIKey = "d5d5298a1a4f079b49e60b8c0e16cbf3"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
