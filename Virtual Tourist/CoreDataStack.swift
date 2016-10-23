7e06dc715b883c3b0975d8dba3420ba09d0497da
Paul Crompton <work@Pauls-MacBook-Pro.local>
2016-10-23 17:08:14 -0400

    Updated Core Data Stack and initialized Pin

--- Virtual Tourist/CoreDataStack.swift
+++ Virtual Tourist/CoreDataStack.swift
@@ -13,60 +13,50 @@ struct CoreDataStack {
     // MARK:  - Properties
     private let model : NSManagedObjectModel
     private let coordinator : NSPersistentStoreCoordinator
-    private let modelURL : NSURL
-    private let dbURL : NSURL
+    private let modelURL : URL
+    private let dbURL : URL
     let context : NSManagedObjectContext
     
     // MARK:  - Initializers
     init?(modelName: String){
         
         // Assumes the model is in the main bundle
-        guard let modelURL = NSBundle.mainBundle().URLForResource(modelName, withExtension: "momd") else {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
             print("Unable to find \(modelName)in the main bundle")
             return nil}
         
         self.modelURL = modelURL
         
         // Try to create the model from the URL
-        guard let model = NSManagedObjectModel(contentsOfURL: modelURL) else{
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else{
             print("unable to create a model from \(modelURL)")
             return nil
         }
         self.model = model
         
-        
-        
         // Create the store coordinator
         coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
         
         // create a context and add connect it to the coordinator
-        context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         context.persistentStoreCoordinator = coordinator
         
-        
-        
         // Add a SQLite store located in the documents folder
-        let fm = NSFileManager.defaultManager()
+        let fm = FileManager.default
         
-        guard let  docUrl = fm.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first else{
+        guard let  docUrl = fm.urls(for: .documentDirectory, in: .userDomainMask).first else{
             print("Unable to reach the documents folder")
             return nil
         }
         
-        self.dbURL = docUrl.URLByAppendingPathComponent("model.sqlite")
-        
-
+        self.dbURL = docUrl.appendingPathComponent("model.sqlite")
+       
         do{
-            try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
+            try addStoreCoordinator(storeType: NSSQLiteStoreType, configuration: nil, storeURL: dbURL as NSURL, options: nil)
             
         }catch{
             print("unable to add store at \(dbURL)")
         }
-        
-        
-        
-        
-        
     }
     
     // MARK:  - Utils
@@ -75,29 +65,20 @@ struct CoreDataStack {
                              storeURL: NSURL,
                              options : [NSObject : AnyObject]?) throws{
         
-        try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: dbURL, options: nil)
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL as URL, options: nil)
         
     }
-}
 
-
-// MARK:  - Removing data
-extension CoreDataStack  {
-    
-    func dropAllData() throws{
+    // MARK:  - Removing data
+    func dropAllData() throws {
         // delete all the objects in the db. This won't delete the files, it will
         // just leave empty tables.
-        try coordinator.destroyPersistentStoreAtURL(dbURL, withType:NSSQLiteStoreType , options: nil)
+        try coordinator.destroyPersistentStore(at: dbURL, ofType: NSSQLiteStoreType, options: nil)
         
-        try addStoreCoordinator(NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
-
-        
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL, options: nil)
     }
-}
 
-// MARK:  - Save
-extension CoreDataStack {
-    
+    // MARK:  - Save
     func saveContext() throws{
         if context.hasChanges {
             try context.save()
@@ -114,13 +95,13 @@ extension CoreDataStack {
                 print("Error while autosaving")
             }
             
-            
             let delayInNanoSeconds = UInt64(delayInSeconds) * NSEC_PER_SEC
-            let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInNanoSeconds))
-            
-            dispatch_after(time, dispatch_get_main_queue(), {
-                self.autoSave(delayInSeconds)
+            let time = DispatchTime(uptimeNanoseconds: delayInNanoSeconds)
+
+            DispatchQueue.main.asyncAfter(deadline: time, execute: { 
+                self.autoSave(delayInSeconds: delayInSeconds)
             })
+  
             
         }
     }
--- Virtual Tourist/Pin+CoreDataClass.swift
+++ Virtual Tourist/Pin+CoreDataClass.swift
@@ -10,5 +10,12 @@ import Foundation
 import CoreData
 
 public class Pin: NSManagedObject {
-
+    
+    convenience init(latitude: Double, longitude: Double, context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 }
